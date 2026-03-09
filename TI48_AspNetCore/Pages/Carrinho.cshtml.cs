10d0a74142bb93eb7961214b72ce417b9b475449
enzo.fmmenacho <enzo.fmmenacho@JUN0570946W10-1>
2026-03-09 17:10:07 -0300

    Add project files.

---
+++ TI48_AspNetCore/Pages/Carrinho.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TI48_AspNetCore.Pages
+{
+    public class CarrinhoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
